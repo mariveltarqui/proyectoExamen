a4f264595b28d4cd7aae09cdfaade420f98f0603
maribeltarqui <147779358+mariveltarqui@users.noreply.github.com>
2024-05-12 19:08:18 -0400

    CRUD registro paciente

--- rutas/registropacienteRutas.js
+++ rutas/registropacienteRutas.js
@@ -2,7 +2,7 @@ const express = require('express');
 const rutas = express.Router();
 const RegistropacienteModel = require('../models/registropaciente');
 
-//endpoint traer todas las recetas
+//endpoint 1 traer todas los registro
 rutas.get('/traerTodoRegistro', async (req, res) => {
     try  {
        const registro = await  RegistropacienteModel.find();
@@ -11,5 +11,58 @@ rutas.get('/traerTodoRegistro', async (req, res) => {
         res.status(500).json({mensaje: error.message});
     }
 });
+//endpoint 2 crear registro
+rutas.post('/crearRegistro',async (req, res)=> {
+    const registro =new RegistropacienteModel({
+        nombres_propietario: req.body.nombres_propietario,
+        apellido_paterno: req.body.apellido_paterno,
+        apellido_materno: req.body.apellido_materno,
+        telefono: req.body.telefono,
+        direccion: req.body.direccion,
+        nombre_mascota: req.body.nombre_mascota,
+        especie: req.body.especie,
+        edad: req.body.edad,
+        raza: req.body.raza,
+        sexo: req.body.sexo
+    })
+    
+    try {
+        const nuevaRegistro = await registro.save();
+        res.status(201).json(nuevaRegistro);
+    }catch (error) {
+        res.status(400).json({mensaje: error.message})
+    }
+    
+    });
+
+    //endpoint 3 editar registro
+rutas.put('/editarRegistro/:id', async (req, res) => {
+    try {
+        const registroEditado = await RegistropacienteModel.findByIdAndUpdate(req.params.id, req.body, {new : true});
+        if (!registroEditado)
+            return res.status(404).json({ mesaje : 'Registro no encontrado!'});
+        else
+            return res.status(201).json(registroEditado);
+    } catch (error){
+        res.status(400).json({ mensaje : error.mensaje})
+    }
+});
+
+//endpoint 4 eliminar registro
+rutas.delete('/eliminarRegistro/:id', async (req, res) =>{
+    try {
+        const registroEliminada = await RegistropacienteModel.findByIdAndDelete(req.params.id);
+        if (!registroEliminada)
+            return res.status(404).json({ mesaje : 'Registro no encontrado!'});
+        else
+            return res.json({mesaje : 'Registro eliminado'});
+    } 
+    catch (error){
+        res.status(500).json({ mensaje : error.mensaje})
+    }
+
+})
+
+
 
 module.exports = rutas;
